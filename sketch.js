9c22f4e38b1d88d9081def69a31db2b3b9831eba
Tanmay <68492733+TANAGG@users.noreply.github.com>
2021-01-12 21:24:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+var Food,database,foodStock;
+var position,lastfed;
+var dog,dogHappy,dogImg,feedDog;
+
+function preload()
+{
+  dogHappy = loadImage("images/dogImg1.png");
+  dogImg = loadImage("images/dogImg.png");
+  
+}
+
+function setup() {
+  createCanvas(1000, 400);
+  
+  dog = createSprite(800,200,160,150);
+  dog.addImage(dogImg)
+  dog.scale = 0
+  database = firebase.database()
+  foodObj = new Food()
+  foodStock = database.ref("food")
+  foodStock.on("value",readStock)
+  feed = createButton("feed the dog")
+  feed.position(700,95)
+  feed.mousePressed(feedDog)
+  addFood = createButton("add food")
+  addFood.position(800,95)
+  addFood.mousePressed(addfoods)
+}
+
+
+function draw() {  
+  background("red")
+  foodObj.display()
+  var fedtime = database.ref("feedtime")
+  fedtime.on("value",function(data){
+    lastfed = data.val();
+  })
+  fill(255,255,254)
+  textSize(15)
+  if(lastfed>=12){
+    text("Last Feed"+ lastfed%12 + "PM",350,30)
+  }else if(lastfed = 0){
+     text("Last Feed : 12 AM",350,30)
+  }else{
+    text("Last Feed :"+ lastfed +"AM",350,30)
+  }
+  drawSprites();
+  
+
+
+
+}
+
+function readStock(data){
+  foods = data.val();
+  foodObj.updateFoodStock(foods)
+}
+
+
+
+  
+
+  function feedDog(){
+    dog.addImage(happyDog);
+
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1)
+    database.ref('/').update({
+      food : foodObj.getFoodStock(),
+      feedTime: hour()
+    })
+  }
+
+
+
